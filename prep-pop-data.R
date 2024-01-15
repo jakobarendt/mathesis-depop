bd483890d4ab2c4ee530b6c0032dd59ed7d954f4
Jakob Arendt <78620868+jakobarendt@users.noreply.github.com>
2024-01-15 05:52:25 +0100

    Merge shapefiles and population data of special case countries
    Before the merge, their projections are transformed. Afterwards,
    the geometry characteristics are aggregated to one single column.

--- prep-pop-data.R
+++ prep-pop-data.R
@@ -5,6 +5,7 @@
 # Packages ----------------------------------------------------------------
 
 require(tidyverse)
+require(s2)
 require(sf)
 
 
@@ -44,4 +45,33 @@ tr <- read_sf('data/shapefiles/tr')
 # Joining historic LAU population with geolocation data -------------------
 
 # Special case: Greece
-gr <- gr |> select(NSI_CODE, geometry)
+gr <- gr |> select(NSI_CODE, SHAPE_Area, geometry) |>
+  mutate(NSI_CODE = paste0("EL", NSI_CODE)) |>
+  rename("GR__SHAPE_Area" = SHAPE_Area)
+gr <- gr |> st_transform("OGC:CRS84")
+hist_pop <- left_join(hist_pop, gr, by = join_by(CNTR_LAU_CODE == NSI_CODE))
+
+# Special case: Ireland
+ie <- ie |> select(MERG_COD, Shape_Area, geometry) |>
+  mutate(MERG_COD = paste0("IE", MERG_COD)) |>
+  rename("IE__SHAPE_Area" = Shape_Area)
+ie <- ie |> st_transform("OGC:CRS84")
+hist_pop <- left_join(hist_pop, ie, by = join_by(CNTR_LAU_CODE == MERG_COD))
+
+# Special case: Turkey
+tr <- tr |> select(ICC_LAU_CO, SHAPE_AREA, geometry) |>
+  rename("TR__SHAPE_Area" = SHAPE_AREA)
+tr <- tr |> st_transform("OGC:CRS84")
+hist_pop <- left_join(hist_pop, tr, by = join_by(CNTR_LAU_CODE == ICC_LAU_CO))
+
+# Consolidate all three geometry columns to one
+hist_pop <- hist_pop |>
+  mutate(geometry = if_else(st_is_empty(geometry.x), geometry, geometry.x)) |>
+  mutate(geometry = if_else(st_is_empty(geometry.y), geometry, geometry.y)) |>
+  select(-geometry.x, -geometry.y)
+# how do I maintain that it is a sf collection even with a left_join? Will the CRS characteristics stay?
+
+# Join shapefiles remaining majority of countries
+# with left_join, but also reduce columns in shapes_files beforehand
+# then also consolidate all geometry columns into one column
+# and work through NA's
