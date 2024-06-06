79aa50cd2e9278572d04540e6a3f228dd9690df1
Jakob Arendt <78620868+jakobarendt@users.noreply.github.com>
2024-06-06 22:58:35 +0200

    Code for down-sampling of weather data on local machine, first grid-sf join
    After many failed attempts of resampling on CDS servers, decided
    to implement temporal down-sampling of daily weather data on local
    machine. This down-sampling procedure to ensure temporal corre-
    spondance to the decade-wise population data is split into various
    steps to reduce computational and memory load on the machine.
    
    First test implementation of spatial join of shapefiles and grid
    data. So far, runs smoothly and surpisingly quick.

--- join-pop-weather-data.R
+++ join-pop-weather-data.R
@@ -25,23 +25,39 @@ if (!file.exists('data/temp/population.RData')) {
   load('data/temp/population.RData')
 }
 # Weather data
-if (!file.exists('data/temp/weather.tif')) {
+if (!file.exists('data/temp/weather.nc')) {
   stop('Please run prep-weather-data.R before running this script.\n
-       I recommend running it as a background job as it takes a long time.')
+       I recommend running it as a background job as it takes a long time.\n
+       For download, a registration with the CDS service is necessary:\n
+       https://cds.climate.copernicus.eu/user/register \n
+       The script then needs username and API key saved, which can with the\n
+       following command that should be ran before the prep-weather-data.R script.\n
+       user <- wf_set_key(service = "cds")')
 } else {
-  weather <- rast('data/temp/weather.tif')
+  weather <- rast('data/temp/weather.nc')
 }
 
+
+
+# Verify that coordinate reference systems of all data correspond ---------
+# st_read() for shape files
+# crs() |> cat() for raster data
 
 
 # Spatial join of population and weather data -----------------------------
 
 # Define function for spatial join/overlay of the grid data onto the shape files
-spatial_join <- function(raster, grid) {
-  joined_data <- exact_extract(raster, grid, fun = mean)
+spatial_join <- function(raster, sf) {
+  joined_data <- exact_extract(raster, sf, fun = mean)
   return(joined_data)
 }
 
+panel_data <- exact_extract(weather,
+                            population,
+                            fun = 'mean',
+                            append_cols = names(population)[-(14:17)],
+                            progress = TRUE)
+panel_data <- panel_data |> as_tibble()
 
 
 # panel_data <-
--- prep-weather-data.R
+++ prep-weather-data.R
@@ -2,21 +2,26 @@
 
 
 
-# Packages ----------------------------------------------------------------
+# Packages and settings ---------------------------------------------------
 
 require(ecmwfr)
 require(terra)
 require(tidyverse)
+require(parallel)
+
+# Set number of cores for parallelization in operations further below
+# Use max amount of cores the hardware provides while keeping 2 for other applications
+co <- detectCores() - 2
 
 
 
 # Download weather data ---------------------------------------------------
 
 # set a key to the keychain interactively
+# (commented out, otherwise script won't run as background job)
 # user <- wf_set_key(service = "cds")
 
-
-
+# Set specifications for grid data set and download
 request <- list(
   dataset_short_name = "insitu-gridded-observations-europe",
   product_type = "ensemble_mean",
@@ -29,49 +34,118 @@ request <- list(
 )
 zipfile_mean_temp_precipitation <- wf_request(request, user, path = 'data/weather', time_out = 7200)
 
+# Unzip downloaded ZIP file
+zipfile_mean_temp_precipitation |>
+  unzip(overwrite = FALSE, junkpaths = TRUE, exdir = 'data/weather')
 
 
-# # Resample weather data online and download -------------------------------
-#
-# # set a key to the keychain interactively
-# user <- wf_set_key(service = "cds")
-#
-# # Down-sample daily mean temperatures to yearly average on CDS servers
-# # and download them
-# # see https://cran.r-project.org/web/packages/ecmwfr/vignettes/cds_workflow_vignette.html
-# # for detailed explanations
-# code_mean_temp_resample <- readLines('resample-mean-temp.py') |>
-#   paste0(collapse = "\n")
-# request_mean_temp <- list(
-#   code = code_mean_temp_resample,
-#   kwargs = list(placeholder_var = 1),
-#   workflow_name = "download_resample_mean_temp_grid",
-#   target = "mean-temp-resampled.nc"
-# )
-# file_mean_temp_resampled <- wf_request(request_mean_temp, user, path = 'data/weather', time_out = 7200)
-#
-# # Down-sample daily precipitation amounts to yearly sums on CDS servers
-# # and download them
-# # see https://cran.r-project.org/web/packages/ecmwfr/vignettes/cds_workflow_vignette.html
-# # for detailed explanations
-# code_precipitation_resample <- readLines('resample-precipitation.py') |>
-#   paste0(collapse = "\n")
-# request_precipitation <- list(
-#   code = code_precipitation_resample,
-#   kwargs = list(placeholder_var = 1),
-#   workflow_name = "download_resample_precipitation_grid",
-#   target = "precipitation-resampled.nc"
-# )
-# file_precipitation_resampled <- wf_request(request_precipitation,
-#                                            user,
-#                                            path = 'data/weather',
-#                                            time_out = 7200)
-#
-#
-#
-# # Join all raster data ----------------------------------------------------
-#
-# weather <- c(rast(file_mean_temp_resampled),
-#              rast(file_precipitation_resampled))
-# dir.create("data/temp")
-# writeRaster(weather, 'data/temp/weather.tif')
+
+# Verify coordinate reference systems of downloaded raster data -----------
+
+# Daily mean temperature
+rast("data/weather/tg_ens_mean_0.1deg_reg_v29.0e.nc") |>
+  crs() |>
+  cat()
+
+# Daily precipitation amount
+rast("data/weather/rr_ens_mean_0.1deg_reg_v29.0e.nc") |>
+  crs() |>
+  cat()
+
+
+
+# Load, resample to yearly values, and combine raster data ----------------
+
+# Resampling for all variables is split into multiple steps to reduce
+# computationl load
+
+# Daily mean temperature - first half of full time period (01/01/1951-31/12/1980):
+# Load daily mean temperature raster data into workspace
+mean_temperature <- rast("data/weather/tg_ens_mean_0.1deg_reg_v29.0e.nc")
+# Subset to first half of full time period
+mean_temperature <- mean_temperature |>
+  subset(time(mean_temperature) >= as.Date("1951-01-01") &
+           time(mean_temperature) <= as.Date("1980-12-31"))
+# Resample the daily mean temperature: Calculate its yearly mean
+mean_temperature <- mean_temperature |>
+  tapp(index = "years", fun = mean, cores = co)
+# Save resampled grid to hard drive and delete from R workspace to free up space
+mean_temperature |> writeRaster('data/temp/mean-temperature-19510101-19801231.tif')
+rm(mean_temperature)
+
+# Daily mean temperature - second half of full time period (01/01/1981-31/12/2010):
+# Load daily mean temperature raster data into workspace
+mean_temperature <- rast("data/weather/tg_ens_mean_0.1deg_reg_v29.0e.nc")
+# Subset to second half of full time period
+mean_temperature <- mean_temperature |>
+  subset(time(mean_temperature) >= as.Date("1981-01-01") &
+           time(mean_temperature) <= as.Date("2010-12-31"))
+# Resample the daily mean temperature: Calculate its yearly mean
+mean_temperature <- mean_temperature |>
+  tapp(index = "years", fun = mean, cores = co)
+# Save resampled grid to hard drive and delete from R workspace to free up space
+mean_temperature |> writeRaster('data/temp/mean-temperature-19810101-20101231.tif')
+rm(mean_temperature)
+
+
+# Daily precipitation amount - first half of full time period (01/01/1951-31/12/1980):
+# Load daily precipitation amount raster data into workspace
+precipitation_amount <- rast("data/weather/rr_ens_mean_0.1deg_reg_v29.0e.nc")
+# Subset to first half of full time period
+precipitation_amount <- precipitation_amount |>
+  subset(time(precipitation_amount) >= as.Date("1951-01-01") &
+           time(precipitation_amount) <= as.Date("1980-12-31"))
+# Resample the daily precipitation amount: Calculate its yearly sum
+precipitation_amount <- precipitation_amount |>
+  tapp(index = "years", fun = sum, cores = co)
+# Save resampled grid to hard drive and delete from R workspace to free up space
+precipitation_amount |> writeRaster('data/temp/precipitation-amount-19510101-19801231.tif')
+rm(precipitation_amount)
+
+# Daily precipitation amount - second half of full time period (01/01/1981-31/12/2010):
+# Load daily precipitation amount raster data into workspace
+precipitation_amount <- rast("data/weather/rr_ens_mean_0.1deg_reg_v29.0e.nc")
+# Subset to second half of full time period
+precipitation_amount <- precipitation_amount |>
+  subset(time(precipitation_amount) >= as.Date("1981-01-01") &
+           time(precipitation_amount) <= as.Date("2010-12-31"))
+# Resample the daily precipitation amount: Calculate its yearly sum
+precipitation_amount <- precipitation_amount |>
+  tapp(index = "years", fun = sum, cores = co)
+# Save resampled grid to hard drive and delete from R workspace to free up space
+precipitation_amount |> writeRaster('data/temp/precipitation-amount-19810101-20101231.tif')
+rm(precipitation_amount)
+
+
+# Load resampled raster data bank into R workspace and combine into SpatRasterDataset
+weather <- sds(
+  c(rast('data/temp/mean-temperature-19510101-19801231.tif'),
+    rast('data/temp/mean-temperature-19810101-20101231.tif')),
+  c(rast('data/temp/precipitation-amount-19510101-19801231.tif'),
+    rast('data/temp/precipitation-amount-19810101-20101231.tif'))
+)
+weather |>
+  names() <- c("mean-daily-mean-temperature", "sum-daily-precipitation-amount")
+
+
+
+# Resample all variables to decade-wise means -----------------------------
+
+yrs <- as.integer(time(weather$`mean-daily-mean-temperature`))
+i <- ((yrs %% 10) == 0)
+yrs[i] <- yrs[i] - 9
+yrs <- paste0("Y", yrs, "_", yrs+10)
+
+yrs <- seq(from = 1951, to = 2010, by = 10) |> rep(each = 10)
+yrs <- paste0("Y", yrs, "_", yrs + 10)
+
+weather <- sds(
+  tapp(weather$`mean-daily-mean-temperature`, index = yrs, fun = mean, cores = co),
+  tapp(weather$`sum-daily-precipitation-amount`, index = yrs, fun = mean, cores = co)
+)
+weather |>
+  names() <- c("mean-daily-mean-temperature", "sum-daily-precipitation-amount")
+
+dir.create("data/temp")
+weather |>
+  writeCDF(filename = "data/temp/weather.nc", overwrite = FALSE)
