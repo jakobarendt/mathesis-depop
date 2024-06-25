23ff30b6d71476958ddf888b9246b4398d3abe9b
Jakob Arendt <78620868+jakobarendt@users.noreply.github.com>
2024-06-25 23:08:07 +0200

    First draft code for reformatting panel data table into long format

--- join-pop-weather-data.R
+++ join-pop-weather-data.R
@@ -1,6 +1,4 @@
 ### This R script joins population and weather data ###
-### It also further aggregates the weather data to temporally correspond to the ###
-### weather data ###
 
 
 
@@ -30,8 +28,9 @@ if (!file.exists('data/temp/weather.nc')) {
        I recommend running it as a background job as it takes a long time.\n
        For download, a registration with the CDS service is necessary:\n
        https://cds.climate.copernicus.eu/user/register \n
-       The script then needs username and API key saved, which can with the\n
-       following command that should be ran before the prep-weather-data.R script.\n
+       The script then needs username and API key saved on the machine, which\n
+       can be done by running the following command before running the
+       prep-weather-data.R script.\n
        user <- wf_set_key(service = "cds")')
 } else {
   weather <- rast('data/temp/weather.nc')
@@ -46,18 +45,50 @@ if (!file.exists('data/temp/weather.nc')) {
 
 # Spatial join of population and weather data -----------------------------
 
-# Define function for spatial join/overlay of the grid data onto the shape files
-spatial_join <- function(raster, sf) {
-  joined_data <- exact_extract(raster, sf, fun = mean)
-  return(joined_data)
-}
-
-panel_data <- exact_extract(weather,
+joined_data <- exact_extract(weather,
                             population,
                             fun = 'mean',
-                            append_cols = names(population)[-(14:17)],
+                            append_cols = names(population)[-(15:17)],
                             progress = TRUE)
-panel_data <- panel_data |> as_tibble()
+joined_data <- panel_data |> as_tibble()
 
+# !! Add na.rm = true to fun = 'mean' ??
+
+
+
+# Reformat table for panel estimations ------------------------------------
+
+panel_data <- joined_data |>
+  select(CNTR_LAU_CODE, ends_with("1_01_01"), starts_with("mean.")) |>
+  pivot_longer(
+    cols = starts_with("POP_") | starts_with("mean."),
+    names_to = c(".value", "date"),
+    names_sep = "_",
+    # names_pattern = "(.*)_(.*)"
+  )
+
+  pivot_longer(starts_with("POP_"), names_to = "YEAR", names_prefix = "POP_",
+               )
+
+
+
+# !! Check what is up with Poian, must be in data set twice
+
+pop |>
+  filter(is.na(POP_1961)) |>
+  leaflet() |>
+  addTiles(options = tileOptions(opacity = 0.2)) |>
+  addPolygons(
+    stroke = FALSE,
+    fillOpacity = 0.7,
+    fillColor = ~pal(POP_2011),
+    label = ~ paste0(LAU_NAME, ": ", round(POP_2011, 4))
+  ) |>
+  addLegend(pal = pal, values = ~POP_2011, opacity = 1.0)
+
+
+population |>
+  select(CNTR_LAU_CODE, geometry) |>
+  right_join(panel_data, by = c("CNTR_LAU_CODE"))
 
 # panel_data <-
